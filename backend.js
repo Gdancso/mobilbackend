4584a5fef471885593026efd0eec685e509fcb28
Gdancso <enyedidani@gmail.com>
2021-12-16 12:54:10 +0100

    gc

--- backend.js
+++ backend.js
@@ -35,6 +35,99 @@ app.get('/statisztika', (req, res) => {
   
 })
 
+app.get('/rend_pont', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'project_m'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT * from statisztika order by statisztika_pont desc', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows);
+    res.send(rows);
+  })
+  
+  connection.end()
+
+
+  
+})
+app.get('/rend_halal', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'project_m'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT * from statisztika order by statisztika_halal desc', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows);
+    res.send(rows);
+  })
+  
+  connection.end()
+
+
+  
+})
+app.get('/rend_ido', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'project_m'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT * from statisztika order by statisztika_ido desc', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows);
+    res.send(rows);
+  })
+  
+  connection.end()
+
+
+  
+})
+app.get('/rend_date', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'project_m'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT * from statisztika order by statisztika_date desc', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows);
+    res.send(rows);
+  })
+  
+  connection.end()
+
+
+  
+})
+
 app.listen(port, () => {
 console.log(`Example app listening at http://localhost:${port}`)
 })
